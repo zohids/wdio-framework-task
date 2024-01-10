261e433f54e8a87a9a75ef05102dfbfc49c73354
zohids <zohidjon.net@gmail.com>
2024-01-10 11:59:28 +0500

    configured wdio config

--- wdio.conf.js
+++ wdio.conf.js
@@ -62,7 +62,10 @@ export const config = {
     // https://saucelabs.com/platform/platform-configurator
     //
     capabilities: [{
-        browserName: 'chrome'
+        browserName: 'chrome',
+        'goog:chromeOptions': {
+            binary: '/usr/bin/google-chrome',
+        }
     }],
 
     //
